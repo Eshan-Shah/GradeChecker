5d8248bfd21e341cb46ddd6a07a0916e9278844d
Eshan Shah <Eshan@Shahs-MacBook-Air.local>
2025-04-10 21:59:51 +0100

    Converted grades to percentages

--- process_data.py
+++ process_data.py
@@ -5,8 +5,13 @@ def find_correct_data(level, board, subject):
     newdata = []
     for item in data:
         if item[0] == level.upper() and item[1] == board.title() and item[2] == subject.title():
-            for n in range(3, len(item)):
-                newdata.append(item[n])
+            maxmark = item[3]
+            print(maxmark)
+            for n in range(4, len(item)):
+                print(item[n])
+                percentage = (item[n] / maxmark) * 100
+                newdata.append(percentage)
+
 
     if newdata != []:
         return newdata
