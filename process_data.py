ed4bff5f63dde93f752e1684471d3bdffd03394a
Eshan Shah <Eshan@Shahs-MacBook-Air.local>
2025-04-11 08:23:18 +0100

    Corresponded percentages to grades for GCSES

--- process_data.py
+++ process_data.py
@@ -4,13 +4,17 @@ from data import data
 def find_correct_data(level, board, subject):
     newdata = []
     for item in data:
-        if item[0] == level.upper() and item[1] == board.title() and item[2] == subject.title():
+        if item[0] == level.upper() and item[1] == board and item[2] == subject.title():
             maxmark = item[3]
-            print(maxmark)
+
+            if item[0] == 'GCSE':
+                grades = ['Grade 9', 'Grade 8', 'Grade 7', 'Grade 6', 'Grade 5', 'Grade 4,', 'Grade 3', 'Grade 2', 'Grade 1']
+            else:
+                grades = ['A*', 'A', 'B', 'C', 'D', 'E,', 'F', '', 'Grade 1']    
+
             for n in range(4, len(item)):
-                print(item[n])
                 percentage = (item[n] / maxmark) * 100
-                newdata.append(percentage)
+                newdata.append([grades[n-4], percentage])
 
 
     if newdata != []:
